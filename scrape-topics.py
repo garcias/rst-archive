41dc3fb82b96bc97d979e11c09734786ed9bbd66
Simon Garcia <garcias@users.noreply.github.com>
2018-04-09 13:01:00 -0400

    rewrite urls in terms of base_url

--- scrape-topics.py
+++ scrape-topics.py
@@ -2,15 +2,17 @@ from urllib2 import urlopen
 from lxml import html, etree
 import json
 
+base_url = 'https://web.archive.org/web/20170426143954/http://www.reducingstereotypethreat.org:80/'
+
 # make a dict mapping each url to the question
-url = 'https://web.archive.org/web/20170426143954/http://www.reducingstereotypethreat.org:80/'
+url = base_url
 doc = html.parse(urlopen(url)).getroot()
 page = doc.xpath('/html/body/table/tr/td/table/tr[5]/td/table')[0]
 
 questions = { a.xpath('.//@href')[0] : a.xpath('./img/@alt')[0] for a in page.xpath('.//a') }
 
 # collect hrefs, topics, and questions
-url = 'https://web.archive.org/web/20170426143954/http://www.reducingstereotypethreat.org:80/definition.html'
+url = base_url + 'definition.html'
 doc = html.parse(urlopen(url)).getroot()
 page = doc.xpath('/html/body/table/tr/td/table/tr[3]/td/table/tr')[0]
 
